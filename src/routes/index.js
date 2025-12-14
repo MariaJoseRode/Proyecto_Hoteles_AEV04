681c1a894e5dadb194e93f5fbe8cc2f59764358d
MJ <maroga06@alumnatflorida.es>
2025-12-14 11:54:36 +0100

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -16,4 +16,24 @@ router.get("/reservas", (req, res) => {
   res.send("Hola desde tu pagina de  reservas");
 });
 
+router.get("/gps", (req, res) => {
+  console.log("Alguien hizo a la pagina de gps");
+  res.send("Hola desde tu pagina del gps");
+});
+
+router.get("/favoritos", (req, res) => {
+  console.log("Alguien hizo a la pagina de favoritos");
+  res.send("Hola desde tus favoritos");
+});
+
+router.get("/ajustes", (req, res) => {
+  console.log("Alguien hizo a la pagina de ajustes");
+  res.send("Hola desde la pagina de ajustes");
+});
+
+router.get("/ajustes", (req, res) => {
+  console.log("Alguien hizo a la pagina de datos de usuario");
+  res.send("Hola desde la pagina de datos de usuario");
+});
+
 module.exports = router;
